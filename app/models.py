5e2e6c85c801c251a4ea7cdce1224a79a950f292
Anton Brichevskiy <brichevskiy1998@mail.ru>
2026-07-28 22:18:43 +0700

    Add CI

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Any, Dict, Optional
 
 from flask_sqlalchemy import SQLAlchemy
 
@@ -8,15 +9,15 @@ db = SQLAlchemy()
 class Client(db.Model):
     __tablename__ = "client"
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    surname = db.Column(db.String(50), nullable=False)
-    credit_card = db.Column(db.String(50))
-    car_number = db.Column(db.String(10))
+    id: int = db.Column(db.Integer, primary_key=True)
+    name: str = db.Column(db.String(50), nullable=False)
+    surname: str = db.Column(db.String(50), nullable=False)
+    credit_card: Optional[str] = db.Column(db.String(50))
+    car_number: Optional[str] = db.Column(db.String(10))
 
     parkings = db.relationship("ClientParking", back_populates="client", lazy="dynamic")
 
-    def to_json(self):
+    def to_json(self) -> Dict[str, Any]:
         return {
             "id": self.id,
             "name": self.name,
@@ -29,15 +30,15 @@ class Client(db.Model):
 class Parking(db.Model):
     __tablename__ = "parking"
 
-    id = db.Column(db.Integer, primary_key=True)
-    address = db.Column(db.String(100), nullable=False)
-    opened = db.Column(db.Boolean, default=True)
-    count_places = db.Column(db.Integer, nullable=False)
-    count_available_places = db.Column(db.Integer, nullable=False)
+    id: int = db.Column(db.Integer, primary_key=True)
+    address: str = db.Column(db.String(100), nullable=False)
+    opened: bool = db.Column(db.Boolean, default=True)
+    count_places: int = db.Column(db.Integer, nullable=False)
+    count_available_places: int = db.Column(db.Integer, nullable=False)
 
     clients = db.relationship("ClientParking", back_populates="parking", lazy="dynamic")
 
-    def to_json(self):
+    def to_json(self) -> Dict[str, Any]:
         return {
             "id": self.id,
             "address": self.address,
@@ -50,11 +51,11 @@ class Parking(db.Model):
 class ClientParking(db.Model):
     __tablename__ = "client_parking"
 
-    id = db.Column(db.Integer, primary_key=True)
-    client_id = db.Column(db.Integer, db.ForeignKey("client.id"))
-    parking_id = db.Column(db.Integer, db.ForeignKey("parking.id"))
-    time_in = db.Column(db.DateTime, default=datetime.utcnow)
-    time_out = db.Column(db.DateTime, nullable=True)
+    id: int = db.Column(db.Integer, primary_key=True)
+    client_id: Optional[int] = db.Column(db.Integer, db.ForeignKey("client.id"))
+    parking_id: Optional[int] = db.Column(db.Integer, db.ForeignKey("parking.id"))
+    time_in: Optional[datetime] = db.Column(db.DateTime, default=datetime.utcnow)
+    time_out: Optional[datetime] = db.Column(db.DateTime, nullable=True)
 
     client = db.relationship("Client", back_populates="parkings")
     parking = db.relationship("Parking", back_populates="clients")
@@ -63,7 +64,7 @@ class ClientParking(db.Model):
         db.UniqueConstraint("client_id", "parking_id", name="unique_client_parking"),
     )
 
-    def to_json(self):
+    def to_json(self) -> Dict[str, Any]:
         return {
             "id": self.id,
             "client_id": self.client_id,
